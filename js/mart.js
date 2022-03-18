21d789b17d3097623eaa95ce823dfb07bb35c80a
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-03-18 15:01:04 -0700

    added cart,envelope and css updated

--- js/mart.js
+++ js/mart.js
@@ -1,9 +1,20 @@
-//order modal
+//Search 
+const f = document.getElementById('form');
+        const q = document.getElementById('query');
+        const google = 'https://www.google.com/search?q=site%3A+';
+        const site = 'pagedart.com';
+
+        function submitted(event) {
+            event.preventDefault();
+            const url = google + site + '+' + q.value;
+            const win = window.open(url, '_blank');
+            win.focus();
+        }
 f.addEventListener('submit', submitted);
 
-$(function () {
-    $("#loginButton").on("click", function () {
-        $("#orderModal").modal('show');
-    });
-})
-
+//order modal
+        $(function () {
+            $("#loginButton").on("click", function () {
+                $("#orderModal").modal('show');
+            });
+        })
